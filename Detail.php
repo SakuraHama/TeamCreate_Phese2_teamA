2ba140cc45375b06917b2f03ed8e0de07742d1cb
Phyo Zaw Aung <phyozawaung673@gmail.com>
2026-06-16 16:13:34 +0900

    last

--- Detail.php
+++ Detail.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
@@ -7,42 +7,43 @@
     <title>詳細画面</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-</head>
-</head>
 
 <body>
+
     <h1>詳細画面</h1>
-    <p>
-        ここに詳細な情報が表示されます。
-    </p>
+    <p>ここに詳細な情報が表示されます。</p>
 
-    <div>
-        <h2>項目1</h2>
-        <input type="checkbox" id="item1" name="items[]" value="">
-    </div>
+    <form action="save.php" method="POST">
 
-    <div>
-        <h2>項目2</h2>
-        <input type="checkbox" id="item2" name="items[]" value="">
-    </div>
+        <div>
+            <input type="checkbox" id="item1" name="items[]" value="1">
+            <label for="item1">項目1</label>
+        </div>
 
-    <div>
-        <h2>項目3</h2>
-        <input type="checkbox" id="item3" name="items[]" value="">
-    </div>
+        <div>
+            <input type="checkbox" id="item2" name="items[]" value="2">
+            <label for="item2">項目2</label>
+        </div>
 
-    <div>
-        <h2>項目4</h2>
-        <input type="checkbox" id="item4" name="items[]" value="">
-    </div>
+        <div>
+            <input type="checkbox" id="item3" name="items[]" value="3">
+            <label for="item3">項目3</label>
+        </div>
 
-    <div>
-        <h2>項目5</h2>
-        <input type="checkbox" id="item5" name="items[]" value="">
-    </div>
+        <div>
+            <input type="checkbox" id="item4" name="items[]" value="4">
+            <label for="item4">項目4</label>
+        </div>
 
-    <a href="Step.php"><button>戻る</button></a>
-    <button type="submit">登録</button>
+        <div>
+            <input type="checkbox" id="item5" name="items[]" value="5">
+            <label for="item5">項目5</label>
+        </div>
+
+        <a href="Step.php" class="btn btn-secondary">戻る</a>
+        <button type="submit" class="btn btn-primary">登録</button>
+
+    </form>
 
 </body>
 
