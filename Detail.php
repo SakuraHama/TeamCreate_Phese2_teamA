c54303bf444ea50c5ad87506e7f41d0bb1e42a24
Phyo Zaw Aung <phyozawaung673@gmail.com>
2026-06-30 14:02:46 +0900

    pull request

--- Detail.php
+++ Detail.php
@@ -67,17 +67,6 @@ try {
 </head>
 
 <body>
-<<<<<<< HEAD
-    <h1>詳細画面</h1>
-    <p>
-        ここに詳細な情報が表示されます。
-    </p>
-
-    <div>
-        <h2>項目1</h2>
-        <input type="checkbox" id="item1" name="items[]" value="">
-=======
-    <!-- Header -->
     <header class="bg-primary text-white py-4 shadow position-relative">
         <div class="container">
             <h1 class="h3">詳細画面</h1>
@@ -112,29 +101,6 @@ try {
                 </div>
             </form>
         </div>
->>>>>>> 328f424e67c180cc24c3bfd28b840c284670074e
-    </div>
-
-    <div>
-        <h2>項目2</h2>
-        <input type="checkbox" id="item2" name="items[]" value="">
-    </div>
-
-    <div>
-        <h2>項目3</h2>
-        <input type="checkbox" id="item3" name="items[]" value="">
-    </div>
-
-    <div>
-        <h2>項目4</h2>
-        <input type="checkbox" id="item4" name="items[]" value="">
-    </div>
-
-    <div>
-        <h2>項目5</h2>
-        <input type="checkbox" id="item5" name="items[]" value="">
-    </div>
-
     <a href="Step.php"><button>戻る</button></a>
     <button type="submit">登録</button>
 
